cce51ba518ae0dc366b0eb56f9dc61975f36d090
Lab.Academico <Lab.Academico@UNS12DT100020.unisul.br>
2022-05-24 20:11:40 -0300

    Projeto inicial

---
+++ SistemaVenda/src/main/java/domain/Pessoa.java
@@ -0,0 +1,38 @@
+package domain;
+
+public class Pessoa {
+
+	private int id;
+	private String nome;
+	
+	
+	public Pessoa() {
+		super();
+	}
+
+	public Pessoa(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
